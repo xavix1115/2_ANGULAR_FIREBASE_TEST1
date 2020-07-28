a9c1dd16c2091e241c17425035cdb2e8a06cb99b
xavix1115 <javierproyectos05@gmail.com>
2020-07-27 18:11:13 -0600

    funciona 18:11

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
     path: 'home',
     loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
   },
